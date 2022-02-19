5827825488b04621a44a22ed236e005f691f7751
Arun Kumar <arunkumarcool324@gmail.com>
2022-02-19 20:15:00 +0530

    Time: 172 ms (52.57%), Space: 89.9 MB (45.93%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -5,11 +5,10 @@ public:
         vector<vector<int>>ans(m,vector<int>(n,0));
         if(size!=m*n)
             return {};
-        int k=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
-                ans[i][j]=original[k++];
+                ans[i][j]=original[n*i+j];
         }
         return ans;
     }
