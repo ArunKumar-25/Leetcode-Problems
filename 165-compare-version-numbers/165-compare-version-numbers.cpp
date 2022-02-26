abadfce751335f5f69bd44df2bd3593469b1ece7
Arun Kumar <arunkumarcool324@gmail.com>
2022-02-26 11:32:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (18.97%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,32 +1,61 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {   
-        int i = 0, j = 0, n1 = version1.size(), n2 = version2.size(), num1 = 0, num2 = 0; 
-        while(i<n1 || j<n2)
+    int compareVersion(string version1, string version2) {
+        vector<int>v1,v2;
+        version1.push_back('.');
+        version2.push_back('.');
+        int i=0,temp=0;
+        while(i<version1.size())
         {
-            while(i<n1 && version1[i]!='.')
+            if(version1[i]=='.')
             {
-                num1 = num1*10+(version1[i]-'0');
-                i++;
+                v1.push_back(temp);
+                temp=0;
             }
-            while(j<n2 && version2[j]!='.')
+            else
             {
-                num2 = num2*10+(version2[j]-'0');
-                j++;
-            }
-            if(num1>num2)
-            {
-                return 1;
-            }
-            if(num1 < num2) 
-            {
-                return -1;
+                temp=temp*10+(version1[i]-'0');
             }
             i++;
-            j++;
-            num1 = 0;
-            num2 = 0;
         }
-        return 0; 
+        i=0,temp=0;
+        while(i<version2.size())
+        {
+            if(version2[i]=='.')
+            {
+                v2.push_back(temp);
+                temp=0;
+            }
+            else
+            {
+                temp=temp*10+(version2[i]-'0');
+            }
+            i++;
+        }
+
+        if(v1.size()!=v2.size())
+        {
+            if(v1.size()<v2.size())
+            {
+                int x=v2.size()-v1.size();
+                while(x--)
+                    v1.push_back(0);
+            }
+            else
+            {
+                int x=v1.size()-v2.size();
+                while(x--)
+                    v2.push_back(0);
+            }
+        }
+        for(int i=0;i<v1.size();i++)
+        {
+            if(v1[i]<v2[i])
+                return -1;
+            if(v1[i]>v2[i])
+                return 1;
+        }
+        return 0;
     }
+    
 };
