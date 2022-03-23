a09c7ffa23d1fcc8fb6bb2a94eb7da728106bd9e
Arun Kumar <arunkumarcool324@gmail.com>
2022-03-23 19:52:20 +0530

    Time: 4 ms (27.86%), Space: 6 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
-        if(startValue>target)
-            return (startValue-target);
-        int cnt=0;
-        while(startValue<target)
-        {
-            if(target%2==0)
-            {
-                target/=2;
-            }
-            else
-                target++;
-            
-            cnt++;
-        }
-        if(startValue>target)
-        {
-            cnt+=abs(startValue-target);
-        }
-        return cnt;
+        if(startValue>=target)
+            return startValue-target;
+        if(target%2==0)
+            return 1+brokenCalc(startValue,target/2);
+        else
+            return 1+brokenCalc(startValue,target+1);
     }
 };
