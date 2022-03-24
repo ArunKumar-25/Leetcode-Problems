6383796ecefcaa7531babdd99962207d2d81765b
Arun Kumar <arunkumarcool324@gmail.com>
2022-03-24 23:19:09 +0530

    Time: 232 ms (23.75%), Space: 111.7 MB (29.92%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,52 +1,25 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        map<int,int>mp1,mp2;
+        vector<int>countA(7),countB(7),countSame(7);
         int n=tops.size();
         for(int i=0;i<n;i++)
         {
-            mp1[tops[i]]++;
-            mp2[bottoms[i]]++;
+            countA[tops[i]]++;
+            countB[bottoms[i]]++;
+            if(tops[i]==bottoms[i])
+                countSame[tops[i]]++;
         }
-        int fir1,fir2,sec1=0,sec2=0;
-        for(auto it:mp1)
+        int res=n;
+        for(int i=1;i<=6;i++)
         {
-            if(it.second>sec1)
+            if(countA[i]+countB[i]-countSame[i]==n)
             {
-                fir1=it.first;
-                sec1=it.second;
+                int mini=min(countA[i],countB[i])-countSame[i];
+                res=min(res,mini);
             }
+            
         }
-        for(auto it:mp2)
-        {
-            if(it.second>sec2)
-            {
-                fir2=it.first;
-                sec2=it.second;
-            }
-        }
-        int res=0;
-        if(sec1>=sec2)
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(bottoms[i]==fir1 && tops[i]!=fir1)
-                    res++;
-                else if(bottoms[i]!=fir1 && tops[i]!=fir1)
-                    return -1;
-            }
-        }
-        else
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(tops[i]==fir2 && bottoms[i]!=fir2)
-                    res++;
-                else if(bottoms[i]!=fir2 && tops[i]!=fir2)
-                    return -1;
-            }
-        }
-        
-        return res;
+        return res==n?-1:res;
     }
 };
