42900bd6d5c12fd7b04f641bfa054de3750b63ff
Arun Kumar <arunkumarcool324@gmail.com>
2022-05-03 01:57:39 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (68.20%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char>st1,st2;
-        int i=0;
-        while(i<s.size())
+        int j=0;
+        for(int i=0;i<s.size();i++)
         {
-            if(s[i]!='#')
-                st1.push(s[i]);
-            else if(!st1.empty())
-                st1.pop();
-            i++;
+            if(s[i]=='#')
+            {
+                j--;
+                if(j<0)
+                    j=0; 
+            }
+            else
+            {
+                s[j]=s[i];
+                j++;
+            }
         }
-        i=0;
-        while(i<t.size())
+        int k=0;
+        for(int i=0;i<t.size();i++)
         {
-            if(t[i]!='#')
-                st2.push(t[i]);
-            else if(!st2.empty())
-                st2.pop();
-            i++;
+            if(t[i]=='#')
+            {
+                k--;
+                if(k<0)
+                    k=0; 
+            }
+            else
+            {
+                t[k]=t[i];
+                k++;
+            }
         }
-        if(st1.size()!=st2.size())
+        if(j!=k)
             return false;
-        while(!st1.empty())
+        for(int i=0;i<k;i++)
         {
-            if(st1.top()!=st2.top())
+            if(s[i]!=t[i])
                 return false;
-            st1.pop();
-            st2.pop();
         }
         return true;
     }
