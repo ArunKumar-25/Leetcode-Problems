4f19ce69bbaa75325d01204fcaa2a69a52f66fdf
Arun Kumar <arunkumarcool324@gmail.com>
2022-06-08 23:00:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+        // ios_base::sync_with_stdio(false);
+        // cin.tie(NULL);
         int i=0,j=s.length()-1;
         while(i<j)
         {
