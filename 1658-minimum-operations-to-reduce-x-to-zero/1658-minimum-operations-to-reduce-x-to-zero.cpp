6dcfd332cc6d904ec085b7fa04a6746ba30274f2
Arun Kumar <arunkumarcool324@gmail.com>
2022-06-11 23:53:59 +0530

    Time: 240 ms (68.28%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-	int minOperations(vector<int>& nums, int x) {
-		int sum = accumulate(nums.begin(), nums.end(), 0);
-		if(sum < x) return -1;
-		if(sum == x) return nums.size();
-		int target = sum - x, currentSum = 0, start = 0, maxSize = 0;
-		for(int i = 0; i < nums.size(); i++) {
-			currentSum += nums[i];
-			while(currentSum > target)
-				currentSum -= nums[start++];
-			if(currentSum == target)
-				maxSize = max(maxSize, i - start + 1);
-		}
-		return (maxSize == 0) ? - 1 : nums.size() - maxSize;
-	}
+    int minOperations(vector<int>& nums, int x) {
+        int totalsum=0,currsum=0,i=0,maxlen=INT_MIN;
+        totalsum=accumulate(nums.begin(),nums.end(),totalsum);
+        int target=totalsum-x;
+        for(int j=0;j<nums.size();j++)
+        {
+            currsum+=nums[j];
+            while(i<=j && currsum>target)
+                currsum-=nums[i++];
+            if(currsum==target)
+                maxlen=max(maxlen,j-i+1);
+        }
+        return maxlen!=INT_MIN?nums.size()-maxlen:-1;
+    }
 };
