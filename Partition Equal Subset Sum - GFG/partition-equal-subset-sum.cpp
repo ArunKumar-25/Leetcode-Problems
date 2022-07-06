2110686c18698a8a3ce4c8284a22086fbd56d8d4
Arun Kumar <arunkumarcool324@gmail.com>
2022-07-06 14:55:56 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,44 +9,43 @@ using namespace std;
 
 class Solution{
 public:
-
-
-   bool solve(int arr[],int sum,int n)
-   {
-       int dp[n+1][sum+1];
-       for(int i=0;i<n+1;i++)
-       {
-           for(int j=0;j<sum+1;j++)
-           {
+    bool solve(int arr[],int sum,int n)
+    {
+        bool dp[n+1][sum+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
                 if(i==0)
                     dp[i][j]=false;
                 if(j==0)
                     dp[i][j]=true;
-           }
-       }
-       for(int i=1;i<n+1;i++)
-       {
-           for(int j=1;j<sum+1;j++)
-           {
+            }
+        }
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
                 if(arr[i-1]<=j)
-                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                {
+                    dp[i][j]= dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
                 else
                     dp[i][j]=dp[i-1][j];
-           }
-       }
-       return dp[n][sum];
-   }
-
-   int equalPartition(int N, int arr[])
-   {
-       int sum=0;
+            }
+        }
+        return dp[n][sum];
+    }
+    int equalPartition(int N, int arr[])
+    {
+        int sum=0;
         for(int i=0;i<N;i++)
             sum+=arr[i];
         if(sum%2!=0)
             return 0;
         
         return solve(arr,sum/2,N);
-   }
+    }
 };
 
 // { Driver Code Starts.
