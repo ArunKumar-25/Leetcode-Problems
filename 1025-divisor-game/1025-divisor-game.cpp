ed2406706ec425b1b0149df520548b373df2d196
Arun Kumar <arunkumarcool324@gmail.com>
2022-07-08 17:09:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (6.89%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
+    int solve(int n,vector<int>&dp)
+    {
+         if(n==1)
+            return 0;
+        if(dp[n]!=-1)
+            return dp[n];
+        else{
+            for(int i=1;i*i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    if(solve(n-i,dp)==0)
+                        return dp[n]=1;
+                    if(i!=1 && solve(n-n/i,dp)==0)
+                        return dp[n]=1;
+                }
+            }
+            return dp[n]=0;
+        }
+    }
+
     bool divisorGame(int n) {
-        return n%2==0;
+       vector<int>dp(n+1,-1);
+       return solve(n,dp);
+
     }
 };
