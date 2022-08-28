05f67e62a1edea764068a4d6a00c8cfdae1df6f4
Arun Kumar <arunkumarcool324@gmail.com>
2022-08-28 15:42:12 +0530

    Time: 23 ms (31.88%), Space: 9.3 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-    void csort(vector<vector<int>>& mat,int r,int c)
-    {
-        int m=mat.size();
-        int n=mat[0].size();
-        int i=r;
-        int j=c;
-        int arr[101]={0};
-        while(i<m && j<n)
-        {
-            arr[mat[i][j]]++;
-            i++;
-            j++;
-        }
-        i=r;
-        j=c;
-        for(int k=1;k<=100;k++)
-        {
-            while(arr[k]>0)
-            {
-                mat[i][j]=k;
-                arr[k]--;
-                i++;
-                j++;
-            }
-        }
-    }
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>>mp;
         int m=mat.size();
         int n=mat[0].size();
         for(int i=0;i<m;i++)
-            csort(mat,i,0);
-        for(int i=1;i<n;i++)
-            csort(mat,0,i);
-        return mat;    
+        {
+            for(int j=0;j<n;j++)
+            {
+                mp[i-j].push(mat[i][j]);
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                mat[i][j]=mp[i-j].top();
+                mp[i-j].pop();
+            }
+        }
+        return mat;
     }
 };
