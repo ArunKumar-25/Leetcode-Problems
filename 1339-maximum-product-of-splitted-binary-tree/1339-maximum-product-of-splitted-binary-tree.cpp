30c2cb11b431455e217cd811ead1d2777403c354
Arun Kumar <arunkumarcool324@gmail.com>
2022-12-11 00:01:44 +0530

    Time: 322 ms (29.13%), Space: 77.3 MB (89.61%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    long long totalSum = 0;
+    long long mod = 1e9+7;
+
+    void TotalSum(TreeNode* root)
+    {
+        if(root==NULL) return;
+
+        TotalSum(root->left);
+        TotalSum(root->right);
+
+        totalSum += root->val;
+    }
+
+    long SubtreeSum(TreeNode* root,long long &maxPro)
+    {
+        if(root==NULL) return 0;
+
+        long l = SubtreeSum(root->left,maxPro);
+        long r = SubtreeSum(root->right,maxPro);
+
+        long curr_sum = l + r + root->val;                                  
+        long curr_Pro = (curr_sum * (totalSum - curr_sum));                 
+
+        if(maxPro < curr_Pro)
+        {
+            maxPro = curr_Pro;
+        }
+
+        return curr_sum;                 
+    }
+
+    int maxProduct(TreeNode* root) 
+    {
+
+        TotalSum(root);
+
+        long long maxPro = 1;
+        SubtreeSum(root,maxPro);
+
+        return (maxPro % mod);
+    }
+};
