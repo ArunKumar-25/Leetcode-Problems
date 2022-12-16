7f9a8431ebb553fb00207efd597add553a9ead51
Arun Kumar <arunkumarcool324@gmail.com>
2022-12-16 05:37:32 +0530

    Added solution - LeetHub

---
+++ Lucky alive person in a circle - GFG/lucky-alive-person-in-a-circle.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int find(int N){
+        // code here
+        int x=1,cnt=0;
+        while(2*x<N)
+        {
+            x*=2;
+            cnt++;
+        }
+        return (N-pow(2,cnt))*2+1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.find(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
