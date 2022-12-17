7badd41dddd64182fb21fd6c769bb40111ba4ba4
Arun Kumar <arunkumarcool324@gmail.com>
2022-12-18 01:42:07 +0530

    Added solution - LeetHub

--- Bit Difference - GFG/bit-difference.cpp
+++ Bit Difference - GFG/bit-difference.cpp
@@ -1,47 +1,49 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
 class Solution{
-    public:
-    // Function to find number of bits needed to be flipped to convert A to B
-    int countBitsFlip(int a, int b){
-        
-        // Your logic here
-        a=(a^b);
-        int cnt=0;
-        while(a!=0)
+public:
+    int countBits(int n, long long int A[])
+    {
+        // code her
+        long long int res=0,mod=1e9+7;
+        for(int i=31;i>=0;i--)
         {
-            int rsb=a&(~a+1);
-            a-=rsb;
-            cnt++;
+            long long int zero=0,one=0;
+            for(int j=0;j<n;j++)
+            {
+                if((A[j]&(1LL<<i))==0)
+                    zero++;
+                else
+                    one++;
+            }
+            res=((one*zero)%mod+res)%mod;
         }
-        return cnt;
-        
+        return (int)((res)%mod*2)%mod;
     }
 };
 
 //{ Driver Code Starts.
 
-// Driver Code
-int main()
-{
-	int t;
-	cin>>t;// input the testcases
-	while(t--) //while testcases exist
-	{
-		int a,b;
-		cin>>a>>b; //input a and b
-
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        long long int A[N];
+        for(int i = 0;i < N; i++)
+            cin>>A[i];
+        
         Solution ob;
-		cout<<ob.countBitsFlip(a, b)<<endl;
-	}
-	return 0;
+        cout<<ob.countBits(N, A)<<"\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
