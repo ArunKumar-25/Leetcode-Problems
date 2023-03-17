feb4c19a19d4e1c26dc4085bf9942e9273518697
Arun Kumar <arunkumarcool324@gmail.com>
2023-03-18 02:14:12 +0530

    Time: 72 ms (51.07%), Space: 48.4 MB (22.34%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,61 @@
+struct TrieNode 
+{
+    char c;
+    bool isWord;
+    vector<TrieNode*> children;
+
+    TrieNode(char x)
+    {
+        c = x;
+        isWord = false;
+        children.resize(26, nullptr);
+    }
+};
+
+class Trie 
+{
+    TrieNode *root;
+public:
+    Trie() 
+    {
+        root = new TrieNode('*');
+    }
+    
+    void insert(string word) 
+    {
+        TrieNode *curr = root;
+        for (int i = 0; i < word.size(); ++i)
+        {
+            if (curr->children[word[i] - 'a'] == nullptr)
+                curr->children[word[i] - 'a'] = new TrieNode(word[i]);
+            curr = curr->children[word[i] - 'a'];
+        }
+        curr->isWord = true;
+    }
+    
+    bool search(string word) 
+    {
+        TrieNode *r = root;
+        for (int i = 0; i < word.size(); ++i)
+        {
+            if (r->children[word[i] - 'a'] == nullptr)
+                return false;
+            else
+                r = r->children[word[i] - 'a'];
+        }
+        return r->isWord;
+    }
+    
+    bool startsWith(string prefix) 
+    {
+        TrieNode *r = root;
+        for (int i = 0; i < prefix.size(); ++i)
+        {
+            if (r->children[prefix[i] - 'a'] == nullptr)
+                return false;
+            else
+                r = r->children[prefix[i] - 'a'];
+        }
+        return true;
+    }
+};
